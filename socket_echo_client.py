7624c11b8dfbf6e5e85cf6862962623448abf64d
Francisca <francisca.ramirez.12@sansano.usm.cl>
2019-09-23 15:39:20 -0300

    Client

--- socket_echo_client.py
+++ socket_echo_client.py
@@ -8,7 +8,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #se escoje un puerto para enviar la informacion.
 sock.bind(('0.0.0.0',50001))
 # Connectar el socket con el puerto en el que el servidor esta escuchando
-server_address = ('192.168.43.113', 1234)
+server_address = ('192.168.43.113', 5000)
 print (sys.stderr, 'connecting to %s port %s' % server_address)
 sock.connect(server_address)
 #Recibir confirmacion de Conexion al servidor
@@ -16,13 +16,21 @@ data = sock.recv(64)
 print (sys.stderr, 'received "%s"' % data)
 try:
     # Enviar primer mensaje codificado con utf-8.
-    message1 = 'Hola, carcamo.'
-    print (sys.stderr, 'sending "%s"' % message1)
-    sock.sendall(message1.encode('utf-8'))
-    # Enviar segundo mensaje codificado con utf-8.
-    message2 = 'Que tal?'
-    print (sys.stderr, 'sending "%s"' % message2)
-    sock.sendall(message2.encode('utf-8'))
+    message = 'Mensaje de prueba'
+    print (sys.stderr, 'sending "%s"' % message)
+    sock.sendall(message.encode('utf-8'))
+    
+    # Esperando respuesta
+    data = sock.recv(5000)
+    print('Recibido {!r}'.format(data))
+    message_new = ''
+    while message_new != 'exit':
+        message_new = input()
+        print (sys.stderr, 'sending "%s"' % message_new)
+        sock.sendall(message_new.encode('utf-8'))
+
+        data = sock.recv(5000)
+        print('Recibido {!r}'.format(data))
 
 # Avisar el cierre del socket y cerrarlo.
 finally:

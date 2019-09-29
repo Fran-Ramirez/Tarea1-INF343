02dd1388d2b7f6e75cfa30347fe281db27d0b572
Francisca <francisca.ramirez.12@sansano.usm.cl>
2019-09-29 02:43:29 -0300

    Actividad 1 Completa

--- Actividad1/client/client.py
+++ Actividad1/client/client.py
@@ -2,9 +2,10 @@ import socket
 import sys
 
 import os
-
 #Se crea el archivo de respuestas
-file = open("./respuestas.txt", "w")
+file = open("respuestas.txt", "a")
+file.write("Inicializando cliente...\n")
+print("Inicializando cliente")
 
 # Se crea un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,27 +14,33 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('0.0.0.0',5000))
 # Connectar el socket con el puerto en el que el servidor esta escuchando
 server_address = ('server', 5000)
-print ('connecting to %s port %s' % server_address)
+print ('conectando a %s por el puerto %s' % server_address)
+file.write('conectando a {} por el puerto {}'.format(*server_address) + "\n")
 sock.connect(server_address)
+
 #Recibir confirmacion de Conexion al servidor
 data = sock.recv(64)
 print('Recibido {!r}'.format(data))
-#guardar el mensaje Recibido
-file.write(str(data))
+file.write("Servidor confirma conexion: " + '{!r}'.format(data)+ "\n")
+file.close()
 try:
     # Enviar primer mensaje codificado con utf-8.
+    file = open("respuestas.txt", "a")
     message = 'Mensaje de prueba'
-    print ('sending "%s"' % message)
+    print ('Enviando "%s"' % message)
+    file.write("Enviando mensaje de prueba al servidor...\n")
     sock.sendall(message.encode('utf-8'))
 
     # Esperando respuesta
     data = sock.recv(5000)
     print('Recibido {!r}'.format(data))
-    file.write(str(data))
-    message_new = ''
+    file.write("Servidor envia respuesta: " + '{!r}'.format(data)+ "\n")
 
+    file.close()
 # Avisar el cierre del socket y cerrarlo.
 finally:
-    print ('closing socket')
+    file = open("respuestas.txt", "a")
+    print ('cerrando socket')
+    file.write('cerrando socket...\n\n')
     sock.close()
     file.close()

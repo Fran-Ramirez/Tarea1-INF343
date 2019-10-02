23d4a59db2099b9aa0ea8a993bc11bb89f7e3866
Francisca <francisca.ramirez.12@sansano.usm.cl>
2019-10-02 20:36:30 -0300

    Fix act1

--- Actividad1/client/client.py
+++ Actividad1/client/client.py
@@ -1,46 +1,43 @@
 import socket
 import sys
-
 import os
-#Se crea el archivo de respuestas
-file = open("respuestas.txt", "a")
-file.write("Inicializando cliente...\n")
-print("Inicializando cliente")
 
+print("Inicializando cliente")
 # Se crea un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#se escoje un puerto para enviar la informacion.
-sock.bind(('0.0.0.0',5000))
 # Connectar el socket con el puerto en el que el servidor esta escuchando
 server_address = ('server', 5000)
-print ('conectando a %s por el puerto %s' % server_address)
-file.write('conectando a {} por el puerto {}'.format(*server_address) + "\n")
+print ('Conectando a %s por el puerto %s' % server_address)
 sock.connect(server_address)
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+file = open("respuestas.txt", "a")
+file.write('Conectado a '+str(IPAddr)+' por el puerto '+str(server_address[1])+'\n')
+file.close()
+print('Conectado a '+str(IPAddr)+' por el puerto '+str(server_address[1]))
 
 #Recibir confirmacion de Conexion al servidor
 data = sock.recv(64)
-print('Recibido {!r}'.format(data))
-file.write("Servidor confirma conexion: " + '{!r}'.format(data)+ "\n")
+print('Servidor confirma conexion: \n recibido {!r}'.format(data))
+file = open("respuestas.txt", "a")
+file.write('[Respuesta del servidor]' + '{!r}'.format(data)+ '\n')
 file.close()
 try:
     # Enviar primer mensaje codificado con utf-8.
-    file = open("respuestas.txt", "a")
     message = 'Mensaje de prueba'
     print ('Enviando "%s"' % message)
-    file.write("Enviando mensaje de prueba al servidor...\n")
     sock.sendall(message.encode('utf-8'))
 
     # Esperando respuesta
     data = sock.recv(5000)
-    print('Recibido {!r}'.format(data))
-    file.write("Servidor envia respuesta: " + '{!r}'.format(data)+ "\n")
-
+    print('Servidor confirma recepcion del mensaje: \n recibido' + '{!r}'.format(data))
+    file = open("respuestas.txt", "a")
+    file.write('[Respuesta del servidor]' + '{!r}'.format(data)+ '\n\n')
     file.close()
 # Avisar el cierre del socket y cerrarlo.
 finally:
-    file = open("respuestas.txt", "a")
-    print ('cerrando socket')
-    file.write('cerrando socket...\n\n')
+    print ('Cerrando socket')
     sock.close()
     file.close()
--- Actividad1/server/server.py
+++ Actividad1/server/server.py
@@ -1,18 +1,11 @@
 import socket
 import sys
-
 import os
 from datetime import datetime
 
-#Se crea el archivo de logs
-file = open("log.txt", "a")
-
 #hora logs
 time = datetime.now()
-actual = time.strftime("%H:%M:%S")
-
-file.write('['+ actual+']Iniciando...\n')
-file.close()
+actual = time.strftime("%Y-%m-%d %H:%M:%S")
 
 # Crear un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,42 +15,37 @@ sock.bind(server_address)# Unir el socket al puerto 5000
 # Escuchar mensajes
 sock.listen(1)
 
-file = open("log.txt", "a")
-file.write('['+ actual+']Partiendo en el ip {} puerto {}'.format(*server_address) + '\n')
-file.close()
-
 while True:
     # Esperar una conexion
     print ( 'Esperando una conexion')
-    file = open("log.txt", "a")
-    file.write('['+ actual+']Esperando una conexion...\n')
-    file.close()
     connection, client_address = sock.accept()
+    file = open("log.txt", "a")
+    file.write('['+ actual+']Cliente '+str(client_address[0])+' intentando conectarse\n')
+    file.close()
     try:
-        print ('Conexion desde', client_address)
-        connection.sendall("Estas conectado con server".encode('utf-8'))
         file = open("log.txt", "a")
-        file.write('['+ actual+']Confirmando conexion \n')
+        file.write('['+ actual+']Conexion exitosa con '+str(client_address[0])+'\n')
+        print ('Conexion desde', str(client_address[0]))
+        connection.sendall('Estas conectado con server'.encode('utf-8'))
+        file.write('['+ actual+']Se envia mensaje de confirmacion a '+str(client_address[0])+'\n')
         file.close()
         #Recibe mensajes hasta que no hayan mas
-        while True:
-            data = connection.recv(64)
-            print ('recibido "%s"' % data)
-            file = open("log.txt", "a")
-            file.write('['+ actual+']Mensaje del cliente: {!r}'.format(data) + '\n')
-            if data:
-            	mensaje= f"El mensaje recibido fue {data}"
-            	connection.sendall(mensaje.encode('utf-8'))
-            else:
-                print ('no hay mas mensajes', client_address)
-                break
-            file.write('['+ actual+']Enviando mensaje...\n')
-            file.close()
+        data = connection.recv(64)
+        print ('recibido %s' % data)
+        file = open("log.txt", "a")
+        file.write('['+ actual+']Mensaje recibido de '+str(client_address[0])+' : {!r}'.format(data) + '\n\n')
+        file.close()
+        if data:
+        	mensaje = 'El mensaje recibido de '+str(client_address[0])+' fue '+str(data)+'\n'
+        	connection.sendall(mensaje.encode('utf-8'))
+        else:
+            print ('no hay mas mensajes', client_address)
+            #break
 
     finally:
         # Cierra la conexion.
         file = open("log.txt", "a")
-        file.write("[" + actual + "] Cerrando conexion...\n\n")
+        print("Cerrando conexion...\n\n")
         file.close()
         connection.close()
         #break

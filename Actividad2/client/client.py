e52a5f87df1969863ba1c68ad4d6906f27739dc0
Francisca <francisca.ramirez.12@sansano.usm.cl>
2019-10-03 00:58:00 -0300

    Fix Dockerfile & docker-compose.yml

--- Actividad2/client/client.py
+++ Actividad2/client/client.py
@@ -1,31 +1,41 @@
-# -*- coding: cp1252 -*-
 import socket
 import sys
+import os
 
+print("Inicializando cliente")
 # Se crea un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#se escoje un puerto para enviar la informacion.
-sock.bind(('0.0.0.0',5000))
 # Connectar el socket con el puerto en el que el servidor esta escuchando
 server_address = ('headnode', 5000)
-print ('connecting to %s port %s' % server_address)
+print ('Conectando a %s por el puerto %s' % server_address)
 sock.connect(server_address)
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+file = open("registro_cliente.txt", "a")
+file.write('Conectando a '+str(IPAddr)+' por el puerto '+str(server_address[1])+'\n')
+file.close()
+print('Conectado a '+str(IPAddr)+' por el puerto '+str(server_address[1]))
+
 #Recibir confirmacion de Conexion al servidor
-data = sock.recv(64)
-print ('received "%s"' % data)
+data = sock.recv(1024)
+print('Servidor confirma conexion: \n recibido {!r}'.format(data))
 try:
     # Enviar primer mensaje codificado con utf-8.
     message = 'Mensaje de prueba'
-    print ('sending "%s"' % message)
+    print ('Enviando "%s"' % message)
     sock.sendall(message.encode('utf-8'))
-    
+
     # Esperando respuesta
-    data = sock.recv(5000)
-    print('Recibido {!r}'.format(data))#Escribir en registro_cliente
-    message_new = ''
+    data = sock.recv(1024)
+    print('Servidor envia el siguiente mensaje: \n Recibido ' + '{!r}'.format(data))
+    file = open("registro_cliente.txt", "a")
+    file.write('[Respuesta del servidor]' + '{!r}'.format(data)+ '\n\n')
+    file.close()
 
 # Avisar el cierre del socket y cerrarlo.
 finally:
-    print ('closing socket')
+    print ('Cerrando socket')
     sock.close()
--- Actividad2/headnode/headnode.py
+++ Actividad2/headnode/headnode.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 import random
-import threading 
+import threading
 import time
 import struct
 
@@ -31,6 +31,13 @@ class RepeatedTimer(object):
   def stop(self):
     self._timer.cancel()
     self.is_running = False
+
+#Creacion archivos
+file1 = open("hearbeat_server.txt", "a")
+file2 = open("registro_server.txt", "a")
+file1.close()
+file1.close()
+
 # Crear un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('headnode', 5000)#Definir una address
@@ -56,37 +63,48 @@ def run_check():
             except socket.timeout:
                     break
             else:
-                print (data.decode("utf-8") + "\n")#Heartbeat_register         
-    finally:        
+                print (data.decode("utf-8") + "\n")#Heartbeat_register
+    finally:
         multisock.close()
 
 t=RepeatedTimer(0.3, run_check)
+
+### Conexion cliente ###
 while True:
     # Esperar una conexion
     print ( 'Esperando una conexion')
     connection, client_address = sock.accept()
     try:
-        print ('Conexion desde', client_address)
-        connection.sendall("Estas conectado con Carcamo".encode('utf-8'))
+        print ('Conexion desde', str(client_address[0]))
+        connection.sendall("Estas conectado con headnode".encode('utf-8'))
         #Recibe mensajes hasta que no hayan mas
-        while True:
-            data = connection.recv(64)
-            print ('recibido "%s"' % data)
-            if data:
-                r=random.randint(1,3)
-                datanode_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                datanode_address=('datanode'+r,5001)
-                datanode_sock.connect(datanodea_address)
-                mensaje= f"El mensaje recibido fue {data} enviado al Datanode{r}"
-                data=datanode_sock.recv(5000)
-                if data:
-                    connection.sendall(mensaje.encode('utf-8'))
-                    print('Recibido {!r}'.format(data))#Escribir en registro_server :'Mensaje guardado en Datanode'+r
-            else:
-                print ('no hay mas mensajes', client_address)
-                break
+        data = connection.recv(1024)
+        print ('recibido %s' % data)
+
+        if data:
+            r = random.randint(1,3)
+            print(r)
+            datanode_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            datanode_address=('datanode'+r,5001)
+            datanode_sock.connect(datanodea_address)
+
+            file = open("registro_server.txt", "a")
+            file.write('El cliente '+str(client_address[0])+' envio el mensaje:\n '+str(data)+'\nque fue guardado en el datanode'+r+'\n\n')
+            file.close()
+
+            mensaje = 'El mensaje recibido de '+str(client_address[0])+' fue '+str(data)+'que fue guardado en el datanode'+r+'\n'
+            connection.sendall(mensaje.encode('utf-8'))
+
+            data2=datanode_sock.recv(1024)
+            if data2:
+                connection.sendall(mensaje.encode('utf-8'))
+                print('Recibido {!r}'.format(data2))
+        else:
+            print ('no hay mas mensajes', client_address)
+            break
     finally:
         # Cierra la conexion.
+        print("Cerrando conexion...\n\n")
         connection.close()
         t.stop()
         break

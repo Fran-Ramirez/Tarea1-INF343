cf86d6f934290440af0e174f6ce77ffa365b1f14
Francisca <francisca.ramirez.12@sansano.usm.cl>
2019-10-03 23:57:24 -0300

    fix indentation error & remove files

--- Actividad2/datanode3/datanode3.py
+++ Actividad2/datanode3/datanode3.py
@@ -28,28 +28,28 @@ def multireceiver():
         multisock.sendto('Operativo 3'.encode('utf-8'), address)
 threading.Thread(target=multireceiver).start()
 while True:
-# Esperar una conexion
-print ( 'Esperando una conexion')
-connection, client_address = sock.accept()
-try:
-    print('Conexion desde', client_address)
-    #Recibe mensajes hasta que no hayan mas
-    multidata, address = multisock.recvfrom(1024)
-    if multidata:
-        multisock.sendto(b'Operativo', address)
-    data = connection.recv(1024)
-    print ('recibido "%s"' % data)#Escribir en data.txt
-    file = open("data.txt", "a")
-    file.write('[Mensaje del cliente]'+str(data)+'\n\n')
-    file.close()
-    if data:
-        mensaje= f"El mensaje recibido fue {data}"
-        connection.sendall(mensaje.encode('utf-8'))
-    else:
-        print ('no hay mas mensajes', client_address)
-        break
+    # Esperar una conexion
+    print ( 'Esperando una conexion')
+    connection, client_address = sock.accept()
+    try:
+        print('Conexion desde', client_address)
+        #Recibe mensajes hasta que no hayan mas
+        multidata, address = multisock.recvfrom(1024)
+        if multidata:
+            multisock.sendto(b'Operativo', address)
+        data = connection.recv(1024)
+        print ('recibido "%s"' % data)#Escribir en data.txt
+        file = open("data.txt", "a")
+        file.write('[Mensaje del cliente]'+str(data)+'\n\n')
+        file.close()
+        if data:
+            mensaje= f"El mensaje recibido fue {data}"
+            connection.sendall(mensaje.encode('utf-8'))
+        else:
+            print ('no hay mas mensajes', client_address)
+            break
 
-finally:
-    # Cierra la conexion.
-    connection.close()
-    #break
+    finally:
+        # Cierra la conexion.
+        connection.close()
+        #break
